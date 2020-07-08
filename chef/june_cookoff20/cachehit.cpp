2af825483a13fb4639dc659aa42a2b12675d9ee7
Rehas Sachdeva <aquannie@gmail.com>
2020-07-08 13:28:21 +0530

    start solving june cookoff 20
    https://www.codechef.com/COOK119B

---
+++ chef/june_cookoff20/cachehit.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define DBG(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1) {
+  cerr << name << " : " << arg1 << std::endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args) {
+  const char* comma = strchr(names + 1, ',');cerr.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...);
+}
+
+#define COUT(...) __g(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __g(const char* name, Arg1&& arg1) {
+  cout << arg1 << std::endl;
+}
+template <typename Arg1, typename... Args>
+void __g(const char* names, Arg1&& arg1, Args&&... args) {
+  const char* comma = strchr(names + 1, ',');cout << arg1 << " ";__g(comma+1, args...);
+}
+
+#define TC()  int tc; cin >> tc; for(int tc_ctr=1; tc_ctr<=tc; tc_ctr++)
+
+int main()
+{
+	TC()
+	{
+		int n,b,m,idx,bi,ans=1;
+		cin >> n >> b >> m;
+		cin >> idx;
+		bi = idx/b;
+		for(int i=1; i<m; i++)
+		{
+			cin >> idx;
+			if(bi != idx/b)
+			{
+				ans++;
+				bi = idx/b;
+			}
+		}
+		cout << ans << endl;
+	}
+	return 0;
+}
